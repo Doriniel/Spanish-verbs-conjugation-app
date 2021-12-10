11c1463a49f70577faba0bae3f0db88f42ff423b
Doriniel <doriniel@gmail.com>
2021-12-10 16:55:52 +0300

    123

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,9 +4,8 @@ import S from './Footer.module.css';
 export default function Footer () {
     return (
         <footer className={S.footer}>
-            <p >
-            {/* Footer */}
-            </p>
+            {/* <p >
+            </p> */}
             {/* <Link className={S.link} to='/about'> О приложении </Link>
             <Link className={S.link} to='/contacts'> Контакты</Link> */}
         </footer>
